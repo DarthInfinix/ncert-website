daedd298f1fa6efb814331d1e1173d664e099870
DarthInfinix <princeprata25@gmail.com>
2023-01-11 14:13:01 +0000

    Update 5 files

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,71 +1,75 @@
-import React, { useState } from 'react'
-import homeVector1 from './assets/homepage-vector-1.png'
-import navbarVector from './assets/navbar-vector.png'
-import logo from './assets/image 1.png'
+import React, { useState, useRef } from "react";
+import homeVector1 from "./assets/homepage-vector-1.png";
+import navbarVector from "./assets/navbar-vector.png";
+import { BiMenu } from "react-icons/bi";
+import logo from "./assets/image 1.png";
+
 const Landing = () => {
-  let [menu, setMenu] = useState(false)
+  let hamburgerMenuRef = useRef("");
+
+  let [menu, setMenu] = useState(true);
   function handleClick(e) {
-menu ? document.querySelector('.hamburger-menu').className = 'hamburger-revealed' : document.querySelector('.hamburger-revealed').className = 'hamburger-menu'
-setMenu(!menu)
+    menu
+      ? (hamburgerMenuRef.current.className = "hamburger-revealed")
+      : (hamburgerMenuRef.current.className = "hamburger-menu");
+    setMenu(!menu);
   }
+
   return (
     <>
-    <nav>
-    <div className="hamburger" onClick={handleClick}>
-            <img src="https://picsum.photos/60" alt="borgir" />
-
-      
-      <div className="hamburger-menu">
-        <ul>
-          <li> HOME</li>
-          <li> ABOUT</li>
-          <li> NOTICES</li>
-          <li> CONTACT</li>
-        </ul>
-      </div>
-
-
-    </div>
-      <div className="nav-side-1">
-                  <img src={logo} alt="ncert-logo" className='ncert-logo' />
-    
-    <div className="ncert-name">
-      <span className='ncert'> NCERT</span>
-    </div>
-
-      </div>
-      <div className="nav-side-2">
-        <ul>
-          <li> home</li>
-          <li> home</li>
-          <li> home</li>
-          <li> home</li>
-
-        </ul>
-      </div>
-    </nav>
-      <div className="hero">
-
-        <img src={logo} alt="ncert logo"  className='hero-ncert-logo'/>
+      <nav>
+        <div className="hamburger" onClick={handleClick}>
+          <BiMenu />
         </div>
-        <div className="hero-container-container">
-        <div className="hero-button-container">
+        <div className="hamburger-menu" ref={hamburgerMenuRef}>
           <ul>
-          <li className='pdf-button'>BOOK PDFs
-          <span className='pdf-line'></span>
-          </li>
-          <li className='about-button'>ABOUT US     <span className='about-line'></span>
-</li>
-          <li className='notices'>NOTICES        <span className='notice-line'></span>
-</li>
+            <li> HOME</li>
+            <li> ABOUT</li>
+            <li> NOTICES</li>
+            <li> CONTACT</li>
           </ul>
         </div>
-      
-        </div>
-        
-    
-    </>
-  )
-}
 
-export default Landing
+        <div
+          className="nav-side-1"
+          style={menu ? { display: "inherit" } : { display: "none" }}
+        >
+          <img src={logo} alt="ncert-logo" className="ncert-logo" />
+
+          <div className="ncert-name">
+            <span className="ncert"> NCERT</span>
+          </div>
+        </div>
+        <div className="nav-side-2">
+          <ul>
+            <li> home</li>
+            <li> home</li>
+            <li> home</li>
+            <li> home</li>
+          </ul>
+        </div>
+      </nav>
+      <div className="hero">
+        <img src={logo} alt="ncert logo" className="hero-ncert-logo" />
+      </div>
+      <div className="hero-container-container">
+        <div className="hero-button-container">
+          <ul>
+            <li className="pdf-button">
+              BOOK PDFs
+              <span className="pdf-line"></span>
+            </li>
+            <li className="about-button">
+              ABOUT US <span className="about-line"></span>
+            </li>
+            <li className="notices">
+              NOTICES <span className="notice-line"></span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Landing;
